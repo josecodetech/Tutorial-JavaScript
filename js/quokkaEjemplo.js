d62b968f03d83ab03304cbc7d1c837412eb57f93
Jose Ojeda <paninarotico@gmail.com>
2021-10-24 09:33:22 +0200

    Actualizacion oct 21

---
+++ js/quokkaEjemplo.js
@@ -0,0 +1,9 @@
+numero = 05
+minimo = 10;
+maximo = 20;
+if (numero > minimo && numero < maximo) {
+    console.log("Esta dentro del intervalo");
+}
+else {
+    console.log("No esta en el intervalo");
+}
