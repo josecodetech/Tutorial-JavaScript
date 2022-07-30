87f0b5bc5ca0f9c2d50727ac33f27172ba018bfe
Jose Ojeda <paninarotico@gmail.com>
2022-07-30 09:05:31 +0200

    Calculo IVA

---
+++ ejercicios/scripts/calculaIVA.js
@@ -0,0 +1,13 @@
+//alert("conectado a javascript");
+const inputBase = document.querySelector('#baseImponible');
+let inputIVA = document.querySelector('#IVA');
+let inputTotal = document.querySelector('#total');
+const tipoIVA = 0.21;
+function Calcular() {
+    const base = Number(inputBase.value);
+    const IVA = base * tipoIVA;
+    inputIVA.value = IVA;
+    const total = base + IVA;
+    inputTotal.value = total;
+
+}
